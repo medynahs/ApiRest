22cccfbf1fd0760e9d57b96dc3871822a422358a
aesi <ariane.medina@gft.com>
2021-05-17 22:22:58 -0300

    Método remover do starter

--- ApiRest/src/main/java/com/ariane/apirest/ApiRest/resource/StarterResource.java
+++ ApiRest/src/main/java/com/ariane/apirest/ApiRest/resource/StarterResource.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +59,12 @@ public class StarterResource {
     public ResponseEntity<Starter> buscarPeloId(@PathVariable Long id) {
         Optional<Starter> starter = starterRepository.findById(id);
 		return starter.isPresent() ? ResponseEntity.ok(starter.get()) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long id){
+        starterRepository.deleteById(id);
     }
 
 }
